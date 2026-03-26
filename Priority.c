0070e1696b03294f237cb77e853cf8455a6024da
Keerthana G Pillai <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2026-03-26 23:54:18 +0530

    Implement priority scheduling for processes

---
+++ Priority.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    int AT[n], BT[n], PR[n], WT[n], TAT[n], CT[n], done[n];
+
+    for(int i=0;i<n;i++){
+        printf("\nProcess %d\n",i+1);
+        scanf("%d%d%d",&AT[i],&BT[i],&PR[i]);
+        done[i]=0;
+    }
+
+    int completed=0, time=0, totalWT=0, totalTAT=0;
+
+    while(completed<n){
+        int minP=INT_MAX, index=-1;
+
+        for(int i=0;i<n;i++){
+            if(AT[i]<=time && !done[i] && PR[i]<minP){
+                minP=PR[i];
+                index=i;
+            }
+        }
+
+        if(index==-1){
+            time++;
+        } else {
+            WT[index]=time-AT[index];
+            time+=BT[index];
+            CT[index]=time;
+            TAT[index]=CT[index]-AT[index];
+
+            totalWT+=WT[index];
+            totalTAT+=TAT[index];
+
+            done[index]=1;
+            completed++;
+        }
+    }
+
+    printf("\nProcess AT BT PR WT TAT\n");
+    for(int i=0;i<n;i++)
+        printf("%d      %d  %d  %d  %d  %d\n",i+1,AT[i],BT[i],PR[i],WT[i],TAT[i]);
+
+    printf("\nAverage WT = %.2f",(float)totalWT/n);
+    printf("\nAverage TAT = %.2f",(float)totalTAT/n);
+
+    return 0;
+}
